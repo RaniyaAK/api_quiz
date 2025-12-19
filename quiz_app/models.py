6d2b6abc386202bb894c23e472f14d95bad456d9
Raniya <raniyaak4@gmail.com>
2025-12-19 10:40:49 +0530

    updated

--- quiz_app/models.py
+++ quiz_app/models.py
@@ -6,10 +6,8 @@ class Quiz(models.Model):
     option_b = models.CharField(max_length=100)
     option_c = models.CharField(max_length=100)
     option_d = models.CharField(max_length=100)
-    answer = models.CharField(
-        max_length=1, 
-        choices=[('A', 'Option A'), ('B', 'Option B'), ('C', 'Option C'), ('D', 'Option D')]
-    )
+    answer = models.CharField(max_length=1, 
+                              choices=[('A', 'Option A'), ('B', 'Option B'), ('C', 'Option C'), ('D', 'Option D')])
 
     def __str__(self):
         return self.question
--- quiz_app/views.py
+++ quiz_app/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from . models import Quiz
 from . serializers import QuizSerializer
-from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from django.db.models import Q
 from rest_framework import status
